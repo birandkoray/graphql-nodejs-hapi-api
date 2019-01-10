ecc5a98f73242f8dffa7b5932ab1c139c2a43a69
birandkoray <birandkoray@gmail.com>
2019-01-10 21:38:42 +0300

    apollo client example added

---
+++ client/src/index.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import * as serviceWorker from './serviceWorker';
+import ApolloClient from "apollo-boost";
+import gql from "graphql-tag";
+import { ApolloProvider , Query } from "react-apollo";
+
+const client = new ApolloClient({
+	uri: "http://localhost:4000/graphql"
+})
+
+
+const ExchangeRates = () => (
+  <Query
+    query={gql`
+      {
+        allPaintings {
+          id name url
+        }
+      }
+    `}
+  >
+    {({ loading, error, data }) => {
+      if (loading) return <p>Loading...</p>;
+      if (error) return <p>Error :(</p>;
+
+      return data.allPaintings.map(({ id, name , url }) => (
+        <div key={id}>
+          <p>{name}: {url}</p>
+        </div>
+      ));
+    }}
+  </Query>
+
+)
+
+const App = () => (
+	<ApolloProvider client={client}>
+		<div>
+			<h2>My first Apollo app <span role="img" aria-label="rocket">🚀</span></h2>
+			<ExchangeRates/>
+		</div>
+	</ApolloProvider>
+)
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
